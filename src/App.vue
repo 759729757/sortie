927b17bb14cb8d7eda9b369df7d201e73ea9a0eb
759729757 <759729757@qq.com>
2020-05-28 17:30:43 +0800

    'nav'

--- src/App.vue
+++ src/App.vue
@@ -12,11 +12,13 @@ export default {
 
 <style>
 #app {
-  font-family: Favorit SSENSE Inter1,PingFang SC,冬青黑体,Hiragino Sans GB,微软雅黑体,Microsoft YaHei,YaHei,sans-serif;
+  font-family: 微软雅黑体,Favorit SSENSE Inter1,PingFang SC,冬青黑体,Hiragino Sans GB,微软雅黑体,Microsoft YaHei,YaHei,sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
 }
+a{text-decoration: none;color: #606266;;}
+img{max-width: 100%;}
   .container{
     max-width: 1200px;
     margin: 0 auto;
--- src/components/nav.vue
+++ src/components/nav.vue
@@ -2,7 +2,7 @@
   <div class="header container">
     <div class="nav-bar">
       <div class="search">
-
+        <el-input v-model="searchInput" placeholder="搜索"></el-input>
       </div>
       <div class="icon">
         <a href="javascript:void(0);">
@@ -10,12 +10,32 @@
         </a>
       </div>
       <div class="options">
-        
+        <el-select v-model="langValue" placeholder="请选择" size="mini">
+          <el-option
+            v-for="item in langOptions"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+        </el-select>
+        <a class="line" href="">登录</a>
+        <a class="line" href="">我的订单</a>
 
       </div>
     </div>
     <div class="nav-tab-bar">
-
+      <div class="tab">
+        关于我们
+      </div>
+      <div class="tab">
+        电子刊
+      </div>
+      <div class="tab">
+        咨询活动
+      </div>
+      <div class="tab">
+        商店
+      </div>
     </div>
   </div>
 
@@ -23,7 +43,28 @@
 
 <script>
     export default {
-        name: "nav"
+        name: "nav",
+      data(){
+          return{
+            searchInput: '',
+            langOptions: [
+              {
+              value: 'Chinese',
+              label: '中文'
+              }, {
+                value: 'English',
+                label: 'English'
+              },
+            ],
+            langValue: 'Chinese',
+          }
+      },
+      mounted(){
+
+      },
+      methods:{
+
+      }
     }
 
 
@@ -33,10 +74,30 @@
   .header{
     margin-top: 10px;margin-bottom: 10px;
   }
+  .icon{
+    width: 150px;
+  }
+  .search{text-align: left;}
+  .options{
+    font: 400 12px Arial;
+    line-height: 50px;
+  }
+  .search,.options{width: 200px;}
 .nav-bar{
   display: flex;justify-content: space-between;
 }
   .nav-tab-bar{
+    margin-top: 25px;margin-bottom: 25px;
     display: flex;justify-content: center;
+  }
+  .nav-tab-bar .tab{
+    font-weight: bold;margin: 0 20px;font-size: 16px;
+  }
+  .line{margin-right: 10px;}
+ /deep/ .el-input{
+    width: 70px;
+  }
+  /deep/ .el-input__inner{
+    padding-right: 0;border: none;
   }
 </style>
