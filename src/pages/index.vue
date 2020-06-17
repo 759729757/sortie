5ec61001104f14439c6f75767a5499740d0ac696
759729757 <759729757@qq.com>
2020-06-17 16:44:17 +0800

    '2'

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -60,7 +60,37 @@
       <div class="swiper-button-next" slot="navigation"></div>
     </div>
 
+    <div class="container news-warp">
 
+      <div class="item">
+        <img class="news-img" src="../assets/images/news1.jpg" alt="">
+        <p>第一个新闻标题~标题不超过14文字</p>
+      </div>
+      <div class="item">
+        <img class="news-img" src="../assets/images/news2.jpg" alt="">
+        <p>第一个新闻标题~标题不超过14文字</p>
+      </div>
+      <div class="item">
+        <img class="news-img" src="../assets/images/news3.jpg" alt="">
+        <p>第一个新闻标题~标题不超过14文字</p>
+      </div>
+      <div class="item">
+        <img class="news-img" src="../assets/images/news1.jpg" alt="">
+        <p>第一个新闻标题~标题不超过14文字</p>
+      </div>
+      <div class="item">
+        <img class="news-img" src="../assets/images/news2.jpg" alt="">
+        <p>第一个新闻标题~标题不超过14文字</p>
+      </div>
+
+    </div>
+    <!--分页组件-->
+    <el-pagination
+      :page-size="20"
+      :pager-count="11"
+      layout="prev, pager, next"
+      :total="1000">
+    </el-pagination>
   </div>
 </template>
 
@@ -129,5 +159,12 @@
     font-weight: bold;
   }
 
+  .news-warp{display: flex;align-items: center;flex-direction: column;justify-content: center;}
+  .news-warp .item{margin-bottom: 10px;}
+  .news-warp .item:first-child{margin-top: 50px;}
+  .news-warp p{margin: 5px 0 15px 0;}
+  .news-warp .news-img{
+    width: 600px;
+  }
 
 </style>
